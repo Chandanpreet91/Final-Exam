a003018ba3a65cd391327fc29502bdb8862c5879
Chandanpreet91 <kaurchandanpreet91@gmail.com>
2021-01-04 13:12:46 -0800

    added welcome page and bootstrap

--- biddr_client/src/App.js
+++ biddr_client/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import WelcomePage from './components/WelcomePage'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <WelcomePage />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ biddr_client/src/components/WelcomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function WelcomePage() {
+    return (
+        <div class="container-fluid">
+            <h1>Welcome to Our Bidding Site</h1>
+            <p>Going twice. Going once. Sold to biddr!</p>
+        </div>
+    )
+}
+
+export default WelcomePage
